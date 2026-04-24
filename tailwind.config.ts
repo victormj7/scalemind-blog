6c7edc631ffba2306e9b325f1a5181c318f6841a
victormj7 <victor.moratojesus@gmail.com>
2026-04-24 10:33:55 -0300

    fix: corrigir darkMode no tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
-  darkMode: false,
+  darkMode: 'media',
   theme: {
     extend: {
       colors: {
